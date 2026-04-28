72e5cef0c9b15ac516d591428a03c923b5add03f
danijela-zorjan <mrs.z0rjan@gmail.com>
2026-04-28 10:56:19 +0200

    form 1 design

--- BeatSwipe/Program.cs
+++ BeatSwipe/Program.cs
@@ -17,7 +17,7 @@ namespace BeatSwipe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new Form1());
         }
     }
 }
